0269b70b0ca63a6b9147bdb2f22318576a639825
kamlesh kumar <kamleshkumarbca92@gmail.com>
2019-11-24 20:04:47 +0530

    add permission class to restrict user to update details without authenticate

--- profile_api/views.py
+++ profile_api/views.py
@@ -2,7 +2,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework.authentication import TokenAuthentication
+from profile_api import permissions
 from profile_api import serializers
 from profile_api import models
 
@@ -99,4 +100,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """ handle creating and updating profiles """
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
 
