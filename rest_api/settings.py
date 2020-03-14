d0bf4ae9def4ec05e83ca3a79ef323186d8d3f3e
kumarkamlesh <40455605+kumarkamlesh@users.noreply.github.com>
2020-03-14 15:53:31 +0530

    Update settings.py

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6098+leiy%g16=ha+q0d^m^^0l0zyj%afi(0bonan=&8@u^2ke'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
